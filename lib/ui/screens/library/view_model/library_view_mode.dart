436ce59a0a9d906c33aff591977bd4ab7d7d904c
Heng Hakley <Hakleybtb18kh@gmail.com>
2026-03-04 22:15:10 +0700

    4.Implement libraryViewModel

--- lib/ui/screens/library/view_model/library_view_mode.dart
+++ lib/ui/screens/library/view_model/library_view_mode.dart
@@ -2,15 +2,53 @@ import 'package:flutter/material.dart';
 import '../../../../data/repositories/songs/song_repository.dart';
 import '../../../states/player_state.dart';
 import '../../../states/settings_state.dart';
+import '../../../../model/songs/song.dart';
 
 class LibraryViewMode extends ChangeNotifier {
   final SongRepository songRepository;
   final PlayerState playerState;
   final AppSettingsState settingsState;
 
+  List<Song> _songs = [];
+
   LibraryViewMode({
     required this.songRepository,
     required this.playerState,
     required this.settingsState,
-  });
+  }) {
+    playerState.addListener(_onPlayerStateChanged);
+    
+    init();
+  }
+
+  Future<void> init() async {
+    _songs = await songRepository.fetchSongs();
+    notifyListeners();
+  }
+
+  void _onPlayerStateChanged() {
+    notifyListeners();
+  }
+
+  List<Song> get songs => _songs;
+  Song? get currentSong => playerState.currentSong;
+  AppSettingsState get settings => settingsState;
+
+  void playSong(Song song) {
+    playerState.start(song);
+  }
+
+  void stopPlayback() {
+    playerState.stop();
+  }
+
+  bool isPlaying(Song song) {
+    return playerState.currentSong == song;
+  }
+
+  @override
+  void dispose() {
+    playerState.removeListener(_onPlayerStateChanged);
+    super.dispose();
+  }
 }
